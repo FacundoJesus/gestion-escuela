8112048109c1c0dae707455bc607231a0d95cba6
Facundo Citera <facundojesus10@hotmail.com>
2026-09-02 18:50:08 -0300

    ...

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,11 @@ Bloque 4: Funciones
 Bloque 5: Condicionales y validaciones
 Bloque 6: Eventos (Vamos a hacer que la página reaccione)
 */
+
+/* Bloque 1: Variables y tipos de datos
+const-> constante, no se puede cambiar su valor
+let-> variable, se puede cambiar su valor
+var -> variable, se puede cambiar su valor, pero es una forma antigua de declarar variables
+*/
+
+const nombreEscuela = "UTN - Facultad Regional Paraná";
