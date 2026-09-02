bb282b70f95ab626c4ecdf50e6ebaa929f1e521e
Facundo Citera <facundojesus10@hotmail.com>
2026-09-02 18:46:37 -0300

    ...

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,6 @@ Bloque 1: Variables y tipos de datos
 Bloque 2: Arreglos y objetos
 Bloque 3: Pescar los elementos del HTML (del DOM)
 Bloque 4: Funciones
-Bloque 5: 
+Bloque 5: Condicionales y validaciones
+Bloque 6: Eventos (Vamos a hacer que la página reaccione)
 */
